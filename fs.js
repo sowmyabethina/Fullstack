88798b815e16bb68a60edead5c7b91ec364c5f40
sowmyabethina <147240818+sowmyabethina@users.noreply.github.com>
2024-07-31 16:04:56 +0530

    Delete fs.js

--- fs.js
+++
@@ -1,8 +0,0 @@
-var http=require('http');
-var fs=require('fs');
-http.createServer((req,res)=>{
-    fs.readFile('test.txt',(err,data)=>{
-        res.write(data);
-        res.end();
-    })
-}).listen(8080)
