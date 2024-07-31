4d5615a2bd0af790c603d0ce932edba4ea2ad959
sowmyabethina <147240818+sowmyabethina@users.noreply.github.com>
2024-07-31 16:04:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-/** server.js **/
+1)
+/** HTTP **/
 
 var http=require('http');
 
@@ -8,29 +9,48 @@ http.createServer((req,res)=>{
     console.log('server running..')
 }).listen(8080)
 
-/** FILE SYSTEM **/
+2)
+    /** FILE SYSTEM **/
 
-var http=require('http');
-var fs=require('fs');
-http.createServer((req,res)=>{
-    fs.readFile('test.txt',(err,data)=>{
-        res.write(data);
-        res.end();
-    })
-}).listen(8080)
-
+        var http=require('http');
+        var fs=require('fs');
+        http.createServer((req,res)=>{
+            fs.readFile('test.txt',(err,data)=>{
+                res.write(data);
+                res.end();
+            })
+        }).listen(8080)
+::OUTPUT:: Hello 
 ::NOTE:: using read file method it can diplay the information in the file
 
-/** USING APPEND **/
+3)
+    /** USING APPEND **/
 
-var http=require('http');
-var fs=require('fs');
-http.createServer((req,res)=>{
-    fs.appendFile('test.txt',welcome,(err,data)=>{
-        res.write(data);
-        res.end();
-    })
-}).listen(8080)
+        var http=require('http');
+        var fs=require('fs');
+        http.createServer((req,res)=>{
+            fs.appendFile('test.txt',welcome,(err,data)=>{
+                res.write(data);
+                res.end();
+            })
+        }).listen(8080)
 
+::OUTPUT:: Hello welcome
 ::NOTE::
-using append method we can add text directly to the information in the text file 
+Using append method we can add text directly to the information in the text file 
+
+4)
+    /** WRITE **/
+        var http=require('http');
+        var fs=require('fs');
+        http.createServer((req,res)=>{
+            fs.writeFile('test.txt',sasi engg college,(err,data)=>{
+                res.write(data);
+                res.end();
+            })
+        }).listen(8080)
+
+OUTPUT: sasi engg college
+::NOTE::using write we can replace the information in the file
+
+
