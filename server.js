8d308443ca45f0f5546659756699b735c3b556b8
sowmyabethina <147240818+sowmyabethina@users.noreply.github.com>
2024-07-31 15:55:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/** server.js **/
+
 var http=require('http');
 
 http.createServer((req,res)=>{
@@ -5,3 +7,30 @@ http.createServer((req,res)=>{
     res.end();
     console.log('server running..')
 }).listen(8080)
+
+/** FILE SYSTEM **/
+
+var http=require('http');
+var fs=require('fs');
+http.createServer((req,res)=>{
+    fs.readFile('test.txt',(err,data)=>{
+        res.write(data);
+        res.end();
+    })
+}).listen(8080)
+
+::NOTE:: using read file method it can diplay the information in the file
+
+/** USING APPEND **/
+
+var http=require('http');
+var fs=require('fs');
+http.createServer((req,res)=>{
+    fs.appendFile('test.txt',welcome,(err,data)=>{
+        res.write(data);
+        res.end();
+    })
+}).listen(8080)
+
+::NOTE::
+using append method we can add text directly to the information in the text file 
